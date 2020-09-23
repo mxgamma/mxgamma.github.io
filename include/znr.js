e34b2c7319f924ffa259601270d8ef3e21a66c93
mxgamma <mxgamma.main@gmail.com>
2020-09-23 16:58:44 -0500

    Update znr.js

--- include/znr.js
+++ include/znr.js
@@ -1,5 +1,4 @@
-var card = {
-    text:'#000000',
-    background:'#aaaaaa',
-    something_else:'blue'
-};
+var blueCard = new Array()
+blueCard[0] = new Array("Jace, Mirror Mage", "A-", "https://media.wizards.com/2020/znr/en_qB0uNPB5i5.png", "m")
+blueCard[1] = new Array("Thieving Skydiver", "B+", "https://media.wizards.com/2020/znr/en_BmUYlUTeUr.png", "r")
+blueCard[2] = new Array("Inscription of Insight", "B", "https://media.wizards.com/2020/znr/en_vrnj7b21G7.png", "r")
