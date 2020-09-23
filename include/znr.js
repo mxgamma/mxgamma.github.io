dee53df488606ddf9720a3ac3ae559c025d544ca
mxgamma <mxgamma.main@gmail.com>
2020-09-23 18:24:09 -0500

    Update znr.js

--- include/znr.js
+++ include/znr.js
@@ -1,4 +1,9 @@
+var whiteCard = new Array()
+    whiteCard[0] = new Array("Emeria's Call", "S", "https://media.wizards.com/2020/znr/en_UYhm8ThgSr.png", "m")
+    whiteCard[1] = new Array("Thieving Maul of the Skyclaves", "A+", "https://media.wizards.com/2020/znr/en_KbX8BEmlPq.png", "r")
+    whiteCard[2] = new Array("Luminarch Aspirant", "A", "https://media.wizards.com/2020/znr/en_uvIZ7ugSKg.png", "r")
+
 var blueCard = new Array()
-blueCard[0] = new Array("Jace, Mirror Mage", "A-", "https://media.wizards.com/2020/znr/en_qB0uNPB5i5.png", "m")
-blueCard[1] = new Array("Thieving Skydiver", "B+", "https://media.wizards.com/2020/znr/en_BmUYlUTeUr.png", "r")
-blueCard[2] = new Array("Inscription of Insight", "B", "https://media.wizards.com/2020/znr/en_vrnj7b21G7.png", "r")
+    blueCard[0] = new Array("Jace, Mirror Mage", "A-", "https://media.wizards.com/2020/znr/en_qB0uNPB5i5.png", "m")
+    blueCard[1] = new Array("Thieving Skydiver", "B+", "https://media.wizards.com/2020/znr/en_BmUYlUTeUr.png", "r")
+    blueCard[2] = new Array("Inscription of Insight", "B", "https://media.wizards.com/2020/znr/en_vrnj7b21G7.png", "r")
