c5a26fd72e3542c658adda8fb4dfa1fced5b19b1
mxgamma <mxgamma.main@gmail.com>
2020-09-23 22:45:04 -0500

    Update znr.js

--- include/znr.js
+++ include/znr.js
@@ -4,616 +4,616 @@ znrCards[0] =  new Array(
     "Allied Assault",
     "u",
     "https://media.wizards.com/2020/znr/en_WA2BW2iT67.png",
-    100
+    160-100
 )
 znrCards[1] =  new Array(
     "w",
     "Angel of Destiny",
     "m",
     "https://media.wizards.com/2020/znr/en_jHTW07aYxP.png",
-    30
+    160-30
 )
 znrCards[2] =  new Array(
     "w",
     "Angelheart Protector",
     "c",
     "https://media.wizards.com/2020/znr/en_vNUejQnYwk.png",
-    100
+    160-100
 )
 znrCards[3] =  new Array(
     "w",
     "Archon of Emeria",
     "r",
     "https://media.wizards.com/2020/znr/en_XOBfgbMqja.png",
-    80
+    160-80
 )
 znrCards[4] =  new Array(
     "w",
     "Archpriest of Iona",
     "r",
     "https://media.wizards.com/2020/znr/en_jK2t6wS8Ys.png",
-    70
+    160-70
 )
 znrCards[5] =  new Array(
     "w",
     "Attended Healer",
     "u",
     "https://media.wizards.com/2020/znr/en_npyUXLWkxe.png",
-    60
+    160-60
 )
 znrCards[6] =  new Array(
     "w",
     "Canyon Jerboa",
     "u",
     "https://media.wizards.com/2020/znr/en_5OvTPwpYhQ.png",
-    100
+    160-100
 )
 znrCards[7] =  new Array(
     "w",
     "Cliffhaven Sell-Sword",
     "c",
     "https://media.wizards.com/2020/znr/en_twgdW6jnsG.png",
-    100
+    160-100
 )
 znrCards[8] =  new Array(
     "w",
     "Dauntless Unity",
     "c",
     "https://media.wizards.com/2020/znr/en_p1cWLPffOb.png",
-    100
+    160-100
 )
 znrCards[9] =  new Array(
     "w",
     "Disenchant",
     "c",
     "https://media.wizards.com/2020/znr/en_SzyHpKSApM.png",
-    130
+    160-130
 )
 znrCards[10] =  new Array(
     "w",
     "Emeria Captain",
     "u",
     "https://media.wizards.com/2020/znr/en_amcqcpc1jw.png",
-    60
+    160-60
 )
 znrCards[11] =  new Array(
     "w",
     "Emeria's Call",
     "m",
     "https://media.wizards.com/2020/znr/en_UYhm8ThgSr.png",
-    10
+    160-10
 )
 znrCards[12] =  new Array(
     "w",
     "Expedition Healer",
     "c",
     "https://media.wizards.com/2020/znr/en_PEE36Frslq.png",
-    80
+    160-80
 )
 znrCards[13] =  new Array(
     "w",
     "Farsight Adept",
     "c",
     "https://media.wizards.com/2020/znr/en_LMfE8EfXrI.png",
-    80
+    160-80
 )
 znrCards[14] =  new Array(
     "w",
     "Fearless Fledgling",
     "u",
     "https://media.wizards.com/2020/znr/en_xGn9G9hZIN.png",
-    60
+    160-60
 )
 znrCards[15] =  new Array(
     "w",
     "Felidar Retreat",
     "r",
     "https://media.wizards.com/2020/znr/en_jXPJ8Cx6vI.png",
-    30
+    160-30
 )
 znrCards[16] =  new Array(
     "w",
     "Journey to Oblivion",
     "u",
     "https://media.wizards.com/2020/znr/en_w0kjo0hvtH.png",
-    50
+    160-50
 )
 znrCards[17] =  new Array(
     "w",
     "Kabira Outrider",
     "c",
     "https://media.wizards.com/2020/znr/en_G0IqIOqExD.png",
-    100
+    160-100
 )
 znrCards[18] =  new Array(
     "w",
     "Kabira Takedown",
     "u",
     "https://media.wizards.com/2020/znr/en_QwuM1TxgLR.png",
-    80
+    160-80
 )
 znrCards[19] =  new Array(
     "w",
     "Kitesail Cleric",
     "u",
     "https://media.wizards.com/2020/znr/en_HFxVlwk7Hi.png",
-    100
+    160-100
 )
 znrCards[20] =  new Array(
     "w",
     "Kor Blademaster",
     "u",
     "https://media.wizards.com/2020/znr/en_bLzFM3n9TP.png",
-    80
+    160-80
 )
 znrCards[21] =  new Array(
     "w",
     "Kor Celebrant",
     "u",
     "https://media.wizards.com/2020/znr/en_pos6KueFpX.png",
-    90
+    160-90
 )
 znrCards[22] =  new Array(
     "w",
     "Legion Angel",
     "r",
     "https://media.wizards.com/2020/znr/en_YjmX41BEq5.png",
-    50
+    160-50
 )
 znrCards[23] =  new Array(
     "w",
     "Luminarch Aspirant",
     "r",
     "https://media.wizards.com/2020/znr/en_uvIZ7ugSKg.png",
-    30
+    160-30
 )
 znrCards[24] =  new Array(
     "w",
     "Makindi Ox",
     "c",
     "https://media.wizards.com/2020/znr/en_kNuYUeY2qL.png",
-    90
+    160-90
 )
 znrCards[25] =  new Array(
     "w",
     "Makindi Stampede",
     "u",
     "https://media.wizards.com/2020/znr/en_zVz1piesaz.png",
-    80
+    160-80
 )
 znrCards[26] =  new Array(
     "w",
     "Maul of the Skyclaves",
     "r",
     "https://media.wizards.com/2020/znr/en_KbX8BEmlPq.png",
-    20
+    160-20
 )
 znrCards[27] =  new Array(
     "w",
     "Mesa Lynx",
     "c",
     "https://media.wizards.com/2020/znr/en_J9W2HsDWoW.png",
-    100
+    160-100
 )
 znrCards[28] =  new Array(
     "w",
     "Nahiri's Binding",
     "c",
     "https://media.wizards.com/2020/znr/en_buzMtbhl1C.png",
-    60
+    160-60
 )
 znrCards[29] =  new Array(
     "w",
     "Ondu Inversion",
     "r",
     "https://media.wizards.com/2020/znr/en_DAzAw71b09.png",
-    90
+    160-90
 )
 znrCards[30] =  new Array(
     "w",
     "Paired Tactician",
     "u",
     "https://media.wizards.com/2020/znr/en_tgZLi0qlm1.png",
-    80
+    160-80
 )
 znrCards[31] =  new Array(
     "w",
     "Practiced Tactics",
     "c",
     "https://media.wizards.com/2020/znr/en_XAZARNWYDv.png",
-    80
+    160-80
 )
 znrCards[32] =  new Array(
     "w",
     "Pressure Point",
     "c",
     "https://media.wizards.com/2020/znr/en_bEfvTfXBoz.png",
-    110
+    160-110
 )
 znrCards[33] =  new Array(
     "w",
     "Prowling Felidar",
     "c",
     "https://media.wizards.com/2020/znr/en_xDRdslN7nO.png",
-    110
+    160-110
 )
 znrCards[34] =  new Array(
     "w",
     "Resolute Strike",
     "c",
     "https://media.wizards.com/2020/znr/en_kgjITet2g7.png",
-    110
+    160-110
 )
 znrCards[35] =  new Array(
     "w",
     "Sea Gate Banneret",
     "c",
     "https://media.wizards.com/2020/znr/en_Pr2FuwYTXa.png",
-    100
+    160-100
 )
 znrCards[36] =  new Array(
     "w",
     "Sejiri Shelter",
     "u",
     "https://media.wizards.com/2020/znr/en_Sgd771pU66.png",
-    70
+    160-70
 )
 znrCards[37] =  new Array(
     "w",
     "Shepherd of Heroes",
     "c",
     "https://media.wizards.com/2020/znr/en_qCVjS9Sip3.png",
-    70
+    160-70
 )
 znrCards[38] =  new Array(
     "w",
     "Skyclave Apparition",
     "r",
     "https://media.wizards.com/2020/znr/en_vh8EoPO0lw.png",
-    40
+    160-40
 )
 znrCards[39] =  new Array(
     "w",
     "Skyclave Cleric",
     "u",
     "https://media.wizards.com/2020/znr/en_uJD4uG5OLq.png",
-    80
+    160-80
 )
 znrCards[40] =  new Array(
     "w",
     "Squad Commander",
     "r",
     "https://media.wizards.com/2020/znr/en_UKBNEYgCt6.png",
-    40
+    160-40
 )
 znrCards[41] =  new Array(
     "w",
     "Smite the Monstrous",
     "c",
     "https://media.wizards.com/2020/znr/en_i2TK1anUAM.png",
-    100
+    160-100
 )
 znrCards[42] =  new Array(
     "w",
     "Tazeem Raptor",
     "c",
     "https://media.wizards.com/2020/znr/en_m0fA9xBbGW.png",
-    90
+    160-90
 )
 znrCards[43] =  new Array(
     "w",
     "Tazri, Beacon of Unity",
     "m",
     "https://media.wizards.com/2020/znr/en_AHTGw7Wb00.png",
-    20
+    160-20
 )
 znrCards[45-1] =  new Array(
     "u",
     "Anticognition",
     "c",
     "https://media.wizards.com/2020/znr/en_mD0oXFuEMr.png",
-    100
+    160-100
 )
 znrCards[46-1] =  new Array(
     "u",
     "Beyeen Veil",
     "u",
     "https://media.wizards.com/2020/znr/en_OviMM7CKMe.png",
-    80
+    160-80
 )
 znrCards[47-1] =  new Array(
     "u",
     "Bubble Snare",
     "c",
     "https://media.wizards.com/2020/znr/en_4hY7EElBVc.png",
-    60
+    160-60
 )
 znrCards[48-1] =  new Array(
     "u",
     "Cascade Seer",
     "c",
     "https://media.wizards.com/2020/znr/en_dlMJVnVzeE.png",
-    90
+    160-90
 )
 znrCards[49-1] =  new Array(
     "u",
     "Charix, the Raging Isle",
     "r",
     "https://media.wizards.com/2020/znr/en_QrZGHyt7gq.png",
-    70
+    160-70
 )
 znrCards[50-1] =  new Array(
     "u",
     "Chilling Trap",
     "c",
     "https://media.wizards.com/2020/znr/en_fO7v29ZXVl.png",
-    90
+    160-90
 )
 znrCards[51-1] =  new Array(
     "u",
     "Cleric of Chill Depths",
     "c",
     "https://media.wizards.com/2020/znr/en_wJ2D9uGJ5x.png",
-    110
+    160-110
 )
 znrCards[52-1] =  new Array(
     "u",
     "Concerted Defense",
     "u",
     "https://media.wizards.com/2020/znr/en_3mSszi80Hg.png",
-    150
+    160-150
 )
 znrCards[53-1] =  new Array(
     "u",
     "Confounding Conundrum",
     "r",
     "https://media.wizards.com/2020/znr/en_wxjrCikeVr.png",
-    150
+    160-150
 )
 znrCards[54-1] =  new Array(
     "u",
     "Coralhelm Chronicler",
     "r",
     "https://media.wizards.com/2020/znr/en_933CTvZtoZ.png",
-    70
+    160-70
 )
 znrCards[55-1] =  new Array(
     "u",
     "Cunning Geysermage",
     "c",
     "https://media.wizards.com/2020/znr/en_MbBVVGgZMc.png",
-    80
+    160-80
 )
 znrCards[56-1] =  new Array(
     "u",
     "Deliberate",
     "c",
     "https://media.wizards.com/2020/znr/en_alssc8RLRX.png",
-    110
+    160-110
 )
 znrCards[57-1] =  new Array(
     "u",
     "Expedition Diviner",
     "c",
     "https://media.wizards.com/2020/znr/en_StwVwUnst8.png",
-    70
+    160-70
 )
 znrCards[58-1] =  new Array(
     "u",
     "Field Research",
     "c",
     "https://media.wizards.com/2020/znr/en_NhmqwEQiCj.png",
-    80
+    160-80
 )
 znrCards[59-1] =  new Array(
     "u",
     "Glacial Grasp",
     "c",
     "https://media.wizards.com/2020/znr/en_IRQo1Psw2y.png",
-    100
+    160-100
 )
 znrCards[60-1] =  new Array(
     "u",
     "Glasspool Mimic",
     "r",
     "https://media.wizards.com/2020/znr/en_LQJC6w5gP4.png",
-    50
+    160-50
 )
 znrCards[61-1] =  new Array(
     "u",
     "Inscription of Insight",
     "r",
     "https://media.wizards.com/2020/znr/en_vrnj7b21G7.png",
-    60
+    160-60
 )
 znrCards[62-1] =  new Array(
     "u",
     "Into the Roil",
     "c",
     "https://media.wizards.com/2020/znr/en_cwhzCwnFzf.png",
-    80
+    160-80
 )
 znrCards[63-1] =  new Array(
     "u",
     "Jace, Mirror Mage",
     "m",
     "https://media.wizards.com/2020/znr/en_qB0uNPB5i5.png",
-    40
+    160-40
 )
 znrCards[64-1] =  new Array(
     "u",
     "Jwari Disruption",
     "u",
     "https://media.wizards.com/2020/znr/en_V9WMDjkBTU.png",
-    80
+    160-80
 )
 znrCards[65-1] =  new Array(
     "u",
     "Living Tempest",
     "c",
     "https://media.wizards.com/2020/znr/en_raMbtgfXPf.png",
-    100
+    160-100
 )
 znrCards[66-1] =  new Array(
     "u",
     "Lullmage's Domination",
     "u",
     "https://media.wizards.com/2020/znr/en_LIfxUE0j0R.png",
-    50
+    160-50
 )
 znrCards[67-1] =  new Array(
     "u",
     "Maddening Cacophony",
     "r",
     "https://media.wizards.com/2020/znr/en_cYc8zpptVe.png",
-    130
+    160-130
 )
 znrCards[68-1] =  new Array(
     "u",
     "Master of Winds",
     "r",
     "https://media.wizards.com/2020/znr/en_m0BmEvOQP5.png",
-    60
+    160-60
 )
 znrCards[69-1] =  new Array(
     "u",
     "Merfolk Falconer",
     "u",
     "https://media.wizards.com/2020/znr/en_LWRIBb1GiH.png",
-    60
+    160-60
 )
 znrCards[70-1] =  new Array(
     "u",
     "Merfolk Windrobber",
     "u",
     "https://media.wizards.com/2020/znr/en_EBcSBMJUin.png",
-    90
+    160-90
 )
 znrCards[71-1] =  new Array(
     "u",
     "Negate",
     "c",
     "https://media.wizards.com/2020/znr/en_KbTb8VpdGm.png",
-    150
+    160-150
 )
 znrCards[72-1] =  new Array(
     "u",
     "Nimble Trapfinder",
     "r",
     "https://media.wizards.com/2020/znr/en_bAd1FkMpjj.png",
-    70
+    160-70
 )
 znrCards[73-1] =  new Array(
     "u",
     "Risen Riptide",
     "c",
     "https://media.wizards.com/2020/znr/en_MXZd3DfIC4.png",
-    90
+    160-90
 )
 znrCards[74-1] =  new Array(
     "u",
     "Roost of Drakes",
     "u",
     "https://media.wizards.com/2020/znr/en_gw7t2g8wat.png",
-    50
+    160-50
 )
 znrCards[75-1] =  new Array(
     "u",
     "Ruin Crab",
     "u",
     "https://media.wizards.com/2020/znr/en_hqvlyHcwDA.png",
-    100
+    160-100
 )
 znrCards[76-1] =  new Array(
     "u",
     "Sea Gate Restoration",
     "m",
     "https://media.wizards.com/2020/znr/en_7xSETF2QnG.png",
-    80
+    160-80
 )
 znrCards[77-1] =  new Array(
     "u",
     "Sea Gate Stormcaller",
     "m",
     "https://media.wizards.com/2020/znr/en_yum3RDhWGk.png",
-    80
+    160-80
 )
 znrCards[78-1] =  new Array(
     "u",
     "Seafloor Stalker",
     "c",
     "https://media.wizards.com/2020/znr/en_RJEgdM13oC.png",
-    90
+    160-90
 )
 znrCards[79-1] =  new Array(
     "u",
     "Shell Shield",
     "c",
     "https://media.wizards.com/2020/znr/en_pF3qWLBzDg.png",
-    120
+    160-120
 )
 znrCards[80-1] =  new Array(
     "u",
     "Silundi Vision",
     "u",
     "https://media.wizards.com/2020/znr/en_HQnBM2cXvs.png",
-    80
+    160-80
 )
 znrCards[81-1] =  new Array(
     "u",
     "Skyclave Plunder",
     "u",
     "https://media.wizards.com/2020/znr/en_HoLrK5lUPf.png",
-    80
+    160-80
 )
 znrCards[82-1] =  new Array(
     "u",
     "Skyclave Squid",
     "c",
     "https://media.wizards.com/2020/znr/en_0jA7n5Z0Ar.png",
-    100
+    160-100
 )
 znrCards[83-1] =  new Array(
     "u",
     "Sure-Footed Infiltrator",
     "u",
     "https://media.wizards.com/2020/znr/en_Ophki9H2VA.png",
-    80
+    160-80
 )
 znrCards[84-1] =  new Array(
     "u",
     "Tazeem Roilmage",
     "c",
     "https://media.wizards.com/2020/znr/en_hwYeg5tUNK.png",
-    80
+    160-80
 )
 znrCards[85-1] =  new Array(
     "u",
     "Thieving Skydiver",
     "r",
     "https://media.wizards.com/2020/znr/en_BmUYlUTeUr.png",
-    50
+    160-50
 )
 znrCards[86-1] =  new Array(
     "u",
     "Umara Wizard",
     "u",
     "https://media.wizards.com/2020/znr/en_3g1Zusw38L.png",
-    60
+    160-60
 )
 znrCards[87-1] =  new Array(
     "u",
     "Windrider Wizard",
     "u",
     "https://media.wizards.com/2020/znr/en_TSo6B1zR81.png",
-    80
+    160-80
 )
 znrCards[88-1] =  new Array(
     "u",
     "Zulaport Duelist",
     "c",
     "https://media.wizards.com/2020/znr/en_XjIsSBIByf.png",
-    80
+    160-80
 )
 
 //S  - 1
@@ -630,3 +630,18 @@ znrCards[88-1] =  new Array(
 //D  - 12
 //D- - 13
 //F  - 150
+
+//S  - 150
+//A+ - 140
+//A  - 130
+//A- - 120
+//B+ - 110
+//B  - 100
+//B- - 90
+//C+ - 80
+//C  - 70
+//C- - 60
+//D+ - 50
+//D  - 40
+//D- - 30
+//F  - 10
