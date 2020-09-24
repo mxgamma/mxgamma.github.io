eaa230f71fdcd626e0a9ef6f40769d6c769ba3de
mxgamma <mxgamma.main@gmail.com>
2020-09-23 19:06:46 -0500

    Update znr.js

--- include/znr.js
+++ include/znr.js
@@ -1,142 +1,36 @@
-var whiteCard = new Array()
-    whiteCard[0] = new Array(
-        "Emeria's Call",
-        "S",
-        "https://media.wizards.com/2020/znr/en_UYhm8ThgSr.png",
-        "m"
-    )
-    whiteCard[1] = new Array(
-        "Maul of the Skyclaves",
-        "A+",
-        "https://media.wizards.com/2020/znr/en_KbX8BEmlPq.png",
-        "r"
-    )
-    whiteCard[2] = new Array(
-        "Luminarch Aspirant",
-        "A",
-        "https://media.wizards.com/2020/znr/en_uvIZ7ugSKg.png",
-        "r"
-    )
-
-var blueCard = new Array()
-    blueCard[0] = new Array(
-        "Jace, Mirror Mage",
-        "A-",
-        "https://media.wizards.com/2020/znr/en_qB0uNPB5i5.png",
-        "m"
-    )
-    blueCard[1] = new Array(
-        "Thieving Skydiver",
-        "B+",
-        "https://media.wizards.com/2020/znr/en_BmUYlUTeUr.png",
-        "r"
-    )
-    blueCard[2] = new Array(
-        "Inscription of Insight",
-        "B",
-        "https://media.wizards.com/2020/znr/en_vrnj7b21G7.png",
-        "r"
-    )
-
-var blackCard = new Array()
-    blackCard[0] = new Array(
-        "Jace, Mirror Mage",
-        "A-",
-        "https://media.wizards.com/2020/znr/en_qB0uNPB5i5.png",
-        "m"
-    )
-    blackCard[1] = new Array(
-        "Thieving Skydiver",
-        "B+",
-        "https://media.wizards.com/2020/znr/en_BmUYlUTeUr.png",
-        "r"
-    )
-    blackCard[2] = new Array(
-        "Inscription of Insight",
-        "B",
-        "https://media.wizards.com/2020/znr/en_vrnj7b21G7.png",
-        "r"
-    )
-
-var redCard = new Array()
-    redCard[0] = new Array(
-        "Jace, Mirror Mage",
-        "A-",
-        "https://media.wizards.com/2020/znr/en_qB0uNPB5i5.png",
-        "m"
-    )
-    redCard[1] = new Array(
-        "Thieving Skydiver",
-        "B+",
-        "https://media.wizards.com/2020/znr/en_BmUYlUTeUr.png",
-        "r"
-    )
-    redCard[2] = new Array(
-        "Inscription of Insight",
-        "B",
-        "https://media.wizards.com/2020/znr/en_vrnj7b21G7.png",
-        "r"
-    )
-
-var greenCard = new Array()
-    greenCard[0] = new Array(
-        "Jace, Mirror Mage",
-        "A-",
-        "https://media.wizards.com/2020/znr/en_qB0uNPB5i5.png",
-        "m"
-    )
-    greenCard[1] = new Array(
-        "Thieving Skydiver",
-        "B+",
-        "https://media.wizards.com/2020/znr/en_BmUYlUTeUr.png",
-        "r"
-    )
-    greenCard[2] = new Array(
-        "Inscription of Insight",
-        "B",
-        "https://media.wizards.com/2020/znr/en_vrnj7b21G7.png",
-        "r"
-    )
-
-var goldCard = new Array()
-    goldCard[0] = new Array(
-        "Jace, Mirror Mage",
-        "A-",
-        "https://media.wizards.com/2020/znr/en_qB0uNPB5i5.png",
-        "m"
-    )
-    goldCard[1] = new Array(
-        "Thieving Skydiver",
-        "B+",
-        "https://media.wizards.com/2020/znr/en_BmUYlUTeUr.png",
-        "r"
-    )
-    goldCard[2] = new Array(
-        "Inscription of Insight",
-        "B",
-        "https://media.wizards.com/2020/znr/en_vrnj7b21G7.png",
-        "r"
-    )
-
 var znrCards = new Array()
 znrCards[0] =  new Array(
     "w",
     "Allied Assault",
     "u",
     "https://media.wizards.com/2020/znr/en_WA2BW2iT67.png",
-    70
+    10.0
 )
 znrCards[1] =  new Array(
     "w",
     "Angel of Destiny",
     "m",
     "https://media.wizards.com/2020/znr/en_jHTW07aYxP.png",
-    90
+    3.0
 )
 znrCards[2] =  new Array(
     "w",
     "Angelheart Protector",
     "c",
     "https://media.wizards.com/2020/znr/en_vNUejQnYwk.png",
-    70
+    10.0
+)
+znrCards[3] =  new Array(
+    "w",
+    "Archon of Emeria",
+    "r",
+    "https://media.wizards.com/2020/znr/en_XOBfgbMqja.png",
+    8.0
+)
+znrCards[4] =  new Array(
+    "w",
+    "Archpriest of Iona",
+    "r",
+    "https://media.wizards.com/2020/znr/en_jK2t6wS8Ys.png",
+    7.0
 )
