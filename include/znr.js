68990858b83a14fe0217e355aca6a63f99ab510e
mxgamma <mxgamma.main@gmail.com>
2020-09-23 20:23:58 -0500

    Update znr.js

--- include/znr.js
+++ include/znr.js
@@ -4,33 +4,33 @@ znrCards[0] =  new Array(
     "Allied Assault",
     "u",
     "https://media.wizards.com/2020/znr/en_WA2BW2iT67.png",
-    10.0
+    100
 )
 znrCards[1] =  new Array(
     "w",
     "Angel of Destiny",
     "m",
     "https://media.wizards.com/2020/znr/en_jHTW07aYxP.png",
-    3.0
+    30
 )
 znrCards[2] =  new Array(
     "w",
     "Angelheart Protector",
     "c",
     "https://media.wizards.com/2020/znr/en_vNUejQnYwk.png",
-    10.0
+    100
 )
 znrCards[3] =  new Array(
     "w",
     "Archon of Emeria",
     "r",
     "https://media.wizards.com/2020/znr/en_XOBfgbMqja.png",
-    8.0
+    80
 )
 znrCards[4] =  new Array(
     "w",
     "Archpriest of Iona",
     "r",
     "https://media.wizards.com/2020/znr/en_jK2t6wS8Ys.png",
-    7.0
+    70
 )
